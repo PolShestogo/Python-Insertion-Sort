c01604a47f6b524938af55132b0a4afd070e0c34
PolShestogo <99017989+PolShestogo@users.noreply.github.com>
2023-10-29 19:20:21 +0300

    exel

--- Insertsort.py
+++ Insertsort.py
@@ -1,5 +1,7 @@
 import random
 import timeit
+from openpyxl import load_workbook
+from openpyxl import Workbook
 
 def insertSort(arr):
         for i in range (1,len(arr)):
@@ -10,18 +12,19 @@ def insertSort(arr):
                         j=j-1
                 arr[j+1]=t
 
-def main():  
+def exeltable(values):
+        workbook = load_workbook(filename="123.xlsx")
+        #workbook=Workbook()        
+        sheet = workbook.active
+        for i in range (1,len(values)):
+                sheet["A"+str(i)] = values[i][0]
+                sheet["B"+str(i)] = values[i][1]
+        workbook.save(filename="123.xlsx")
+
+def sort(n, bool=False):
         max=1000
         min=-1000
         nmax=100000              
-        try:
-                n= input("Введите количество элементов сортируемого массива: ")
-                if abs(int(n))>=nmax:
-                        print('Слишком большое количество элементов')
-                        exit()
-        except Exception:
-                print("Некорректный ввод")
-                exit() 
         file=open('input.txt','w+')
         file.write(str(random.randint(min,max)))
         for i in range (1, int(n)-1):
@@ -30,14 +33,33 @@ def main():
         file.close()
         file=open('input.txt','r')
         arr=list((file.read()).split())
-        print('Исходный массив:', str(arr))
+        if bool :
+                print('Исходный массив:', str(arr))
         start = timeit.default_timer()
         insertSort(arr)
         end= timeit.default_timer()
-        print('Отсортированный массив: ', arr)
+        if bool :
+                print('Отсортированный массив: ', arr)
         sorted=open('sorted.txt','w+')
         sorted.write(str(arr))
-        print("Время сортировки: ", end-start)
+        t=end-start
+        if bool :
+                print("Время сортировки: ",t )
+        print(n)
+        return(n,t)
 
+
+def main():  
+       #n= input("Введите количество элементов сортируемого массива: ")
+       values=[[]]
+       j=100
+       n=50
+       for i in range(1,j):
+                values.append(sort(int(n)*i))
+       exeltable(values)
+       
+
+
+        
 if __name__ == '__main__':
     main()
